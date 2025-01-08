971982b29d404a19a12bda1923524bb0e76fafaa
= <ethangranegarciafp@gmail.com>
2025-01-08 12:47:59 +0100

    css

--- Views/Layout/Head.php
+++ Views/Layout/Head.php
@@ -9,12 +9,12 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <!-- Styles -->
-<link rel="stylesheet" href="\Views\Styles\MainStyles.css">
-<link rel="stylesheet" href="\Views\Styles\HeaderStyles.css">
-<link rel="stylesheet" href="\Views\Styles\FooterStyles.css">
-<link rel="stylesheet" href="\Views\Styles\MainCard.css">
-<link rel="stylesheet" href="\Views\Styles\HalfCard.css">
-<link rel="stylesheet" href="\Views\Styles\CartStyles.css">
+<link rel="stylesheet" href="/Views/Styles/MainStyles.css">
+<link rel="stylesheet" href="/Views/Styles/HeaderStyles.css">
+<link rel="stylesheet" href="/Views/Styles/FooterStyles.css">
+<link rel="stylesheet" href="/Views/Styles/MainCard.css">
+<link rel="stylesheet" href="/Views/Styles/HalfCard.css">
+<link rel="stylesheet" href="/Views/Styles/CartStyles.css">
 
 <?php
 if (empty($title)) 
